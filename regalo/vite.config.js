f48d5ea9b77afcfd45d8a64453f2289caa739529
thiarape21 <80516444+thiarape21@users.noreply.github.com>
2024-11-16 15:44:46 -0600

    Update vite.config.js

--- regalo/vite.config.js
+++ regalo/vite.config.js
@@ -1,10 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: './', // Si necesitas rutas relativas, ajusta esto
-  build: {
-    outDir: 'dist', // Asegúrate de que Netlify espere este directorio
-  },
+  plugins: [react()],
 });

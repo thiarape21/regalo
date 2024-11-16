401f905db13143e8a66c6bc3d57d17cfb0c077fc
thiarape21 <80516444+thiarape21@users.noreply.github.com>
2024-11-16 15:50:08 -0600

    Revert "Update vite.config.js"
    This reverts commit f48d5ea9b77afcfd45d8a64453f2289caa739529.

--- regalo/vite.config.js
+++ regalo/vite.config.js
@@ -1,6 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: './', // Si necesitas rutas relativas, ajusta esto
+  build: {
+    outDir: 'dist', // Asegúrate de que Netlify espere este directorio
+  },
 });
